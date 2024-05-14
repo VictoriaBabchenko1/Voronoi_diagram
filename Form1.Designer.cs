15c53fbb7e0afb16b6d5df6b3d8884452124169e
Victoria <victoria.babchenko30@gmail.com>
2024-05-14 12:30:56 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Voronoi_diagram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.AmountOfPointsTB = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.AmountOfThreadsTB = new System.Windows.Forms.TextBox();
+            this.GenerateBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.singleRadio = new System.Windows.Forms.RadioButton();
+            this.multiRadio = new System.Windows.Forms.RadioButton();
+            this.ShowPointsCB = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 49);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(29, 103);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 17);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Amount of Points:";
+            // 
+            // AmountOfPointsTB
+            // 
+            this.AmountOfPointsTB.Location = new System.Drawing.Point(162, 98);
+            this.AmountOfPointsTB.Margin = new System.Windows.Forms.Padding(4);
+            this.AmountOfPointsTB.Name = "AmountOfPointsTB";
+            this.AmountOfPointsTB.Size = new System.Drawing.Size(93, 22);
+            this.AmountOfPointsTB.TabIndex = 6;
+            this.AmountOfPointsTB.Text = "10";
+            this.AmountOfPointsTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(29, 160);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(125, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Amount of Treads:";
+            // 
+            // AmountOfThreadsTB
+            // 
+            this.AmountOfThreadsTB.Location = new System.Drawing.Point(162, 155);
+            this.AmountOfThreadsTB.Margin = new System.Windows.Forms.Padding(4);
+            this.AmountOfThreadsTB.Name = "AmountOfThreadsTB";
+            this.AmountOfThreadsTB.Size = new System.Drawing.Size(93, 22);
+            this.AmountOfThreadsTB.TabIndex = 8;
+            this.AmountOfThreadsTB.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // GenerateBtn
+            // 
+            this.GenerateBtn.Location = new System.Drawing.Point(41, 330);
+            this.GenerateBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.GenerateBtn.Name = "GenerateBtn";
+            this.GenerateBtn.Size = new System.Drawing.Size(190, 57);
+            this.GenerateBtn.TabIndex = 9;
+            this.GenerateBtn.Text = "Generate";
+            this.GenerateBtn.UseVisualStyleBackColor = true;
+            this.GenerateBtn.Click += new System.EventHandler(this.GenerateBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(325, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(725, 524);
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // singleRadio
+            // 
+            this.singleRadio.AutoSize = true;
+            this.singleRadio.Location = new System.Drawing.Point(77, 219);
+            this.singleRadio.Margin = new System.Windows.Forms.Padding(4);
+            this.singleRadio.Name = "singleRadio";
+            this.singleRadio.Size = new System.Drawing.Size(111, 21);
+            this.singleRadio.TabIndex = 11;
+            this.singleRadio.TabStop = true;
+            this.singleRadio.Text = "single thread";
+            this.singleRadio.UseVisualStyleBackColor = true;
+            this.singleRadio.CheckedChanged += new System.EventHandler(this.singleRadio_CheckedChanged);
+            // 
+            // multiRadio
+            // 
+            this.multiRadio.AutoSize = true;
+            this.multiRadio.Location = new System.Drawing.Point(77, 248);
+            this.multiRadio.Margin = new System.Windows.Forms.Padding(4);
+            this.multiRadio.Name = "multiRadio";
+            this.multiRadio.Size = new System.Drawing.Size(103, 21);
+            this.multiRadio.TabIndex = 12;
+            this.multiRadio.TabStop = true;
+            this.multiRadio.Text = "multi thread";
+            this.multiRadio.UseVisualStyleBackColor = true;
+            // 
+            // ShowPointsCB
+            // 
+            this.ShowPointsCB.AutoSize = true;
+            this.ShowPointsCB.Location = new System.Drawing.Point(77, 277);
+            this.ShowPointsCB.Margin = new System.Windows.Forms.Padding(4);
+            this.ShowPointsCB.Name = "ShowPointsCB";
+            this.ShowPointsCB.Size = new System.Drawing.Size(104, 21);
+            this.ShowPointsCB.TabIndex = 13;
+            this.ShowPointsCB.Text = "show points";
+            this.ShowPointsCB.UseVisualStyleBackColor = true;
+            this.ShowPointsCB.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1067, 554);
+            this.Controls.Add(this.ShowPointsCB);
+            this.Controls.Add(this.multiRadio);
+            this.Controls.Add(this.singleRadio);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.GenerateBtn);
+            this.Controls.Add(this.AmountOfThreadsTB);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.AmountOfPointsTB);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox AmountOfPointsTB;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox AmountOfThreadsTB;
+        private System.Windows.Forms.Button GenerateBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton singleRadio;
+        private System.Windows.Forms.RadioButton multiRadio;
+        private System.Windows.Forms.CheckBox ShowPointsCB;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Voronoi_diagram
+{
+    public partial class Form1 : Form
+    {
+        private int width = 500;
+        private int height = 400;
+
+        private Random random = new Random();
+
+        private readonly List<Point> _points = new List<Point>();
+        private readonly Dictionary<Point, Color> _cellColors = new Dictionary<Point, Color>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Define the number of points to use
+            
+        }
+
+        private void DrawVoronoiDiagram()
+        {
+            var bmp = new Bitmap(width, height);
+
+            for (var x = 0; x < bmp.Width; x++)
+            {
+                for (var y = 0; y < bmp.Height; y++)
+                {
+                    var closestPoint = FindClosestPoint(new Point(x, y));
+
+                    if (closestPoint != null)
+                    {
+                        bmp.SetPixel(x, y, _cellColors[closestPoint.Value]);
+                    }
+                }
+            }
+
+            if (ShowPointsCB.Checked)
+            {
+                DrawPoints(bmp);
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void DrawVoronoiDiagramMultiThread()
+        {
+            var bmp = new Bitmap(width, height);
+
+            int numThreads;
+            if (AmountOfThreadsTB.Text == String.Empty)
+            {
+                numThreads = Environment.ProcessorCount;
+            }
+            else 
+            {
+                numThreads = Int32.Parse(AmountOfThreadsTB.Text);
+            }
+            int regionWidth = bmp.Width / numThreads;
+            List<Rectangle> regions = new List<Rectangle>();
+            for (int i = 0; i < numThreads; i++)
+            {
+                regions.Add(new Rectangle(i * regionWidth, 0, regionWidth, bmp.Height));
+            }
+
+            var tasks = new List<Task>();
+            foreach (var region in regions)
+            {
+                tasks.Add(Task.Run(() =>
+                {
+                    for (var x = region.X; x < region.X + region.Width; x++)
+                    {
+                        for (var y = region.Y; y < region.Y + region.Height; y++)
+                        {
+                            var closestPoint = FindClosestPoint(new Point(x, y));
+
+                            if (closestPoint != null)
+                            {
+                                lock (_cellColors)
+                                {
+                                    bmp.SetPixel(x, y, _cellColors[closestPoint.Value]);
+                                }
+                            }
+                        }
+                    }
+                }));
+            }
+
+            Task.WaitAll(tasks.ToArray());
+
+            if (ShowPointsCB.Checked)
+            {
+                DrawPoints(bmp);
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        private void DrawPoints(Bitmap bmp)
+        {
+            using (var g = Graphics.FromImage(bmp))
+            {
+                var pointSize = 3;
+                foreach (var point in _points)
+                {
+                    var brush = new SolidBrush(Color.Black);
+                    g.FillEllipse(brush, point.X - pointSize / 2, point.Y - pointSize / 2, pointSize, pointSize);
+                }
+            }
+
+            pictureBox1.Image = bmp;
+        }
+
+        private Point? FindClosestPoint(Point p)
+        {
+            var minDistance = double.MaxValue;
+            Point? closestPoint = null;
+
+            foreach (var point in _points)
+            {
+                var distance = Math.Sqrt(Math.Pow(p.X - point.X, 2) + Math.Pow(p.Y - point.Y, 2));
+                if (distance < minDistance)
+                {
+                    minDistance = distance;
+                    closestPoint = point;
+                }
+            }
+
+            return closestPoint;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            _points.Add(new Point(e.X, e.Y));
+            _cellColors.Add(new Point(e.X, e.Y), Color.FromArgb(random.Next(256), random.Next(256), random.Next(256)));
+            DrawVoronoiDiagram();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GenerateBtn_Click(object sender, EventArgs e)
+        {
+            _points.Clear();
+            _cellColors.Clear();
+            int numPoints = Int32.Parse(AmountOfPointsTB.Text);
+
+            var rand = new Random();
+            for (var i = 0; i < numPoints; i++)
+            {
+                var point = new Point(rand.Next(width), rand.Next(height));
+                _points.Add(point);
+
+                var color = Color.FromArgb(rand.Next(256), rand.Next(256), rand.Next(256));
+                _cellColors[point] = color;
+            }
+
+            if (singleRadio.Checked)
+            {
+                DrawVoronoiDiagram();
+            }
+            else if (multiRadio.Checked)
+            {
+                DrawVoronoiDiagramMultiThread();
+            }
+            
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void singleRadio_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
